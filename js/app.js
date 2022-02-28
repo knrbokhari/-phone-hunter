f3e0f49da643f54f7dfec32a2274d003c3fcc0a0
naeem <kazinaeemrayhan@gmail.com>
2022-03-01 01:54:31 +0600

    3rd commit

--- js/app.js
+++ js/app.js
@@ -1,25 +1,27 @@
 const phoneSearch = async () => {
   const searchInput = document.getElementById("search");
-  searchInput.value = " ";
   const searchText = searchInput.value;
   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  //   console.log(searchText);
+
   const res = await fetch(url);
   const data = await res.json();
   displayResult(data.data);
+  searchInput.value = " ";
 };
 
 const displayResult = (data) => {
   const result = document.getElementById("result");
   data.map((phone) => {
-    // console.log(phone)
+    // console.log(phone);
     const div = document.createElement("div");
-    div.classList.add("col-md-4");
+    div.classList.add("col-md-4", "mb-3");
     div.innerHTML = `
-        <img src="${phone.image}" class="card-img-top">
-        <div class="card-body">
+        <img src="${phone.image}" class="mx-auto d-block w-75">
+        <div class="card-body text-center">
             <h5 class="card-title fs-4">Name: ${phone.phone_name}</h5>
             <p class="card-text fs-5">Brand: ${phone.brand}</p>
-            <a href="#details" onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">Details</a>
+            <a href="#button" onclick="phoneDetails('${phone.slug}')" class="btn btn-primary">Details</a>
         </div>
     `;
     result.appendChild(div);
@@ -34,12 +36,16 @@ const phoneDetails = async (id) => {
 };
 
 const displayDetails = (data) => {
-  console.log(data);
+  //   console.log(data);
   const details = document.getElementById("details");
+  details.innerHTML = "";
   const div = document.createElement("div");
+  div.classList.add("row");
   div.innerHTML = `
-    <img src="${data.image}" class="card-img-top">
-    <div class="card-body">
+    <div class="col-md-4 mb-5">
+        <img src="${data.image}" class="mx-auto d-block w-75">
+    </div>
+    <div class="card-body col-md-8">
         <h5 class="card-title fs-4">Name: ${data.name}</h5>
         <p class="card-text fs-5 m-0">Brand: ${data.brand}.</p>
         <p class="card-text fs-5 m-0">ChipSet: ${data.mainFeatures.chipSet}.</p>
